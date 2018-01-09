fb0641d36e16c206185408a92f76597f8bbc1a58
Вадим Янчарук <vadim.yancharuk@morion.local>
2018-01-09 16:43:28 +0200

    Добавил получение геоокоординат и их отправление.

--- AptekaUaBot/Program.cs
+++ AptekaUaBot/Program.cs
@@ -9,6 +9,7 @@ using Telegram.Bot.Args;
 
 
 
+
 namespace AptekaUaBot
 {
     class Program
@@ -41,12 +42,40 @@ namespace AptekaUaBot
 
                 if (txt == "/start")
                 {
-                    await bot.SendTextMessageAsync(cid, "Упыри так что делать будем?");
+                    await bot.SendTextMessageAsync(cid, "?");
                    
                 }
                 else
                 {
                     await bot.SendTextMessageAsync(cid, txt);
+                }
+            }
+            if (e.Message.Type == MessageType.LocationMessage)
+            {
+                var txt = e.Message.Text;
+                var cid = e.Message.Chat.Id;
+                var name = e.Message.From.FirstName + " " + e.Message.From.LastName;
+                var uid = e.Message.From.Id;
+                var pol = bot.IsReceiving;
+                var Latitude = e.Message.Location.Latitude.ToString();
+                var Longitude = e.Message.Location.Longitude.ToString();
+
+                float Lat = 50.42130f;
+                float Lon = 30.593798f;
+
+
+                Console.WriteLine("{0} - {1} : {2}, {3}, {4}", uid, name, txt, Latitude, Longitude);
+
+                if (txt == "/start")
+                {
+                    await bot.SendTextMessageAsync(cid, "Ближайшая аптека находится по адресу - м.Київ, вул.Березняківська, 31 - НАРОДНА АПТЕКА НОМЕР ОДИН");
+
+                }
+                else
+                {
+                    await bot.SendTextMessageAsync(cid, "Ближайшая аптека находится по адресу - м.Київ, вул.Березняківська, 31 - НАРОДНА АПТЕКА НОМЕР ОДИН");
+                    await bot.SendLocationAsync(cid, Lat, Lon);
+               
                 }
             }
         }
